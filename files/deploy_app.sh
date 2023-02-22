ae9e4b26947673ba00634e8b96967fbbf87cf554
Hari Kishan Beerumota <harikishan467@gmail.com>
2023-02-22 08:56:14 +0530

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to Hari's app. This is done on Github.
   <!-- END -->
 
   </div>
